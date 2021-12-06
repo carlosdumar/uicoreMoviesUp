8fb3fb8ddad221cfce7d26606b17269132528eec
crodriguezd <carlosdumar4@gmail.com>
2021-12-06 13:13:49 -0500

    test: test 7

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -37,7 +37,7 @@ interface ellipsis {
 }
 
 interface TitleProps extends StyledProps {
-  /** className to be appended to default className s */
+  /** className to be appended to default className */
   className?: string
 
   /** Code style */
